6317284a89059f0d3b0a7913cf4a40d500839341
Samer Sawan <67536733+SamerSawan@users.noreply.github.com>
2024-06-29 18:47:14 -0400

    added cool hover effect for cursor on card, but color is not final

--- app/components/projectCard.js
+++ app/components/projectCard.js
@@ -1,10 +1,47 @@
-import TechnicalPoint from "./technicalPoint";
+"use client";
+import TechnicalPoint from "./TechnicalPoint";
+import React, { useEffect } from 'react';
+import "./projectCard.css";
 
 const ProjectCard = ({title, point1, point2, point3, point4, techPoints}) => {
+  useEffect(() => {
+    const cardUpdate = (e) => {
+      const $card = e.currentTarget;
+      var pos = [e.clientX, e.clientY];
+      e.preventDefault();
+      if (e.type === 'touchmove') {
+        pos = [e.touches[0].clientX, e.touches[0].clientY];
+      }
+      var dimensions = $card.getBoundingClientRect();
+      var l = pos[0] - dimensions.left;
+      var t = pos[1] - dimensions.top;
+      var h = dimensions.height;
+      var w = dimensions.width;
+      var px = Math.abs((100 / w) * l);
+      var py = Math.abs((100 / h) * t);
+
+      $card.style.setProperty('--pointer-x', `${px}%`);
+      $card.style.setProperty('--pointer-y', `${py}%`);
+    };
+
+    const cards = document.querySelectorAll('.card');
+
+    cards.forEach(card => {
+      card.addEventListener('mousemove', cardUpdate);
+      card.addEventListener('touchmove', cardUpdate);
+
+      return () => {
+        card.removeEventListener('mousemove', cardUpdate);
+        card.removeEventListener('touchmove', cardUpdate);
+      };
+    });
+  }, []);
+
     return (
-        <div className="mb-10 relative transition hover:bg-[#2E485C]/50 
+        <div className="card mb-10 relative transition hover:bg-[#2E485C]/50 
         hover:bg-opacity-10 hover:backdrop-blur-sm hover:bg-clip-padding 
         backdrop-filter hover:shadow-xl rounded-lg p-6 mx-auto">
+          <div className="inside">
             <h2 className="text-slate-200 font-semibold mb-2">{title}</h2>
             <div className="flex flex-col space-y-2">
                 <p>{point1}</p>
@@ -16,6 +53,7 @@ const ProjectCard = ({title, point1, point2, point3, point4, techPoints}) => {
             {techPoints.map((tech, index) => (
                     <TechnicalPoint key={index} tech={tech} />
                 ))}
+            </div>
             </div>
         </div>
     )
